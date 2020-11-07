9fb349b44ed2a2ba5ed5ae8421d1e313b9da2e16
007steve <stephen.plummer1.sp@gmail.com>
2020-11-07 14:58:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Song from './components/Song';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> Music Player</h1>
+    <Song/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Song.css'
+function Song() {
+    return (
+        <div>
+            <h2>hello</h2>
+        </div>
+    )
+}
+
+export default Song
